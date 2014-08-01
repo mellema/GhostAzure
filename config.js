213582eb2051f4f0bc511a8d5977e93ddd291e87
Walker Mellema <walkermellema@gmail.com>
2014-07-31 19:45:02 -0700

    fix blog URL in config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://www.mellema.io',
         mail: {},
         database: {
             client: 'sqlite3',
