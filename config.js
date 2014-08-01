79dfde62ee255020e25030f436e9521d85781010
Walker Mellema <walkermellema@gmail.com>
2014-07-31 19:47:55 -0700

    add blog URL

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://walkerbmellema.azurewebsites.net',
+        url: 'http://www.mellema.io',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
